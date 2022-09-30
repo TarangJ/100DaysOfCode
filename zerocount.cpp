9b3475014d8d4728e61429ec134fa92fb7c580a8
TarangJ <65700353+TarangJ@users.noreply.github.com>
2022-09-30 20:10:18 +0530

    Add files via upload

---
+++ zerocount.cpp
@@ -0,0 +1,12 @@
+class Solution{   
+public:
+    int countZeroes(int arr[], int n) {
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i] == 0)
+             sum++;
+             
+        }
+        return sum;
+    }
+};
