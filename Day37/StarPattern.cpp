83534ade0c57f62e652e812ae85e515800fb87fc
TarangJ <65700353+TarangJ@users.noreply.github.com>
2022-11-08 20:40:11 +0530

    Add files via upload

---
+++ Day37/StarPattern.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of row for print triangle : "<<"\n";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+	    for(int j=n;j>=i;j--){
+	        cout<<"*"<<" ";
+	    }
+        cout<<"\n";
+    }            
+    return 0;
+}
