7696e9b05697da643f9a07009c9e7e22d15ca1e9
TarangJ <65700353+TarangJ@users.noreply.github.com>
2022-11-11 23:05:33 +0530

    Update ArrayRot.cpp

--- Day23/ArrayRot.cpp
+++ Day23/ArrayRot.cpp
@@ -1,14 +1,18 @@
-class ArrRot{
-    public:
-    void rotateArr(int arr[], int d, int n){
-        int count=1;
-        while(count<=d){
-            int tmp=arr[0];
-            for(int i=1;i<n;i++){
-                arr[i-1]=arr[i];
-            }
-            arr[n-1]=tmp;
-            count++;
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of elements : \n";
+    cin>>n;
+    cout<<"Enter the elements of the array: \n";
+    int arr[n];
+    n =sizeof(arr)/sizeof(arr[0]);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
         }
-    }
-};
+        for(int i=n-1;i>=0;i--){
+            cout<<arr[i]<<" ";
+        }
+
+    return 0;
+}
