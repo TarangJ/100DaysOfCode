5ef4c94d14daf5dba1c4d0e339ffb5225ce2e57a
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2022-11-14 21:41:50 +0530

    Update ArrayMult.cpp

--- Day26/ArrayMult.cpp
+++ Day26/ArrayMult.cpp
@@ -1,9 +1,17 @@
-class ArrayMult{
-    public:
-    int prod(int arr[], int n){
-        for(int i=1;i<n;i++){
-            arr[0]*=arr[i];
-        }
-        return arr[0];
+#include<iostream>
+using namespace std;
+int main(){
+    int n,pro=1;
+    cout<<"Enter the size of the array : \n";
+    cin>>n;
+    int arr[n];
+    n=sizeof(arr)/sizeof(arr[0]);
+    cout<<"Enter the elements of the array : \n";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        pro*=arr[i];
     }
-};
+    cout<<pro;
+
+    return 0;
+}
