eb4936626e9dcd1a4d78290704194602b13276c1
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2022-11-15 18:55:58 +0530

    Update RevStr.cpp

--- Day27/RevStr.cpp
+++ Day27/RevStr.cpp
@@ -1,13 +1,16 @@
-class StrRev {
-  public:
-    string revStr(string S) {
-        // code here
-        string A;
-        A.assign(S);
-        int i,n=S.size()-1;
-        for(i=0;i<=n;i++){
-            S[i]=A[n-i];
-        }
-        return S;
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string S,A;
+    cout<<"Enter the string you want to reverse : \n";
+    cin>>S;
+    A=S;
+    int i,n=S.size()-1;
+    for(i=0;i<=n;i++){
+        S[i]=A[n-i];
     }
-};
+    cout<<"Reversed string : \n"<<S;
+
+    return 0;
+}
