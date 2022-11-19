2aca596acc9deb20aa3c1427235926e2efb7be43
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2022-11-19 19:21:31 +0530

    Update RevStr.cpp

--- Day31/RevStr.cpp
+++ Day31/RevStr.cpp
@@ -1,7 +1,17 @@
-string revWord(string str){
-    
-  for(int i=str.length()-1;i>=0;i--){
-      cout<<str[i];
-  }
-    
+#include<iostream>
+using namespace std;
+int main(){
+    int count=0;
+    string str;
+    cout<<"\n Enter the string : ";
+    cin>>str;
+    cout<<"Reversed string : ";
+    for(int i=str.length()-1;i>=0;i--){
+        cout<<str[i];
+        count++;
+    }
+    cout<<endl;
+    cout<<"No. of letters in string : "<<count;
+
+    return 0;
 }
