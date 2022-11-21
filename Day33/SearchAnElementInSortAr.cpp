97eaaa8e3b0f82c2365ffbd92c10a63d1a8a85d5
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2022-11-21 20:23:55 +0530

    Update SearchAnElementInSortAr.cpp

--- Day33/SearchAnElementInSortAr.cpp
+++ Day33/SearchAnElementInSortAr.cpp
@@ -1,12 +1,20 @@
-class SearchInSortAr{
-    public:
-    int searchInSorted(int arr[], int N, int K) 
-    { 
-        for(int i=0;i<N;i++){
-            if(arr[i]==K){
-                return 1;
-            }
+#include<iostream>
+using namespace std;
+int main(){
+    int n,f;
+    cout<<"Enter the number size of array : ";
+    cin>>n;
+    int arr[n];
+    n=sizeof(arr)/sizeof(arr[0]);
+    cout<<"Enter the number you want to find : ";
+    cin>>f;
+    cout<<"Enter the elements for the array : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+
+        if(arr[i]==f){
+            cout<<"Element found at index : "<<i;
         }
-        return -1;
     }
-};
+    return 0;
+} 
