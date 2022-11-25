6059dbd63a2ef0b40cf424ddb895ad122b862357
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2022-11-25 18:33:02 +0530

    Update StarPattern.cpp

--- Day37/StarPattern.cpp
+++ Day37/StarPattern.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter the number of row for print triangle : "<<"\n";
+    cout<<"Enter the number of row for print triangle : ";
     cin>>n;
     for(int i=1;i<=n;i++){
 	    for(int j=n;j>=i;j--){
