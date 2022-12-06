e9a66d945fff8d468be63a60f2980520bcbc921f
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2022-12-06 19:49:05 +0530

    Update Pattern.cpp

--- Day45/Pattern.cpp
+++ Day45/Pattern.cpp
@@ -1,15 +1,26 @@
+#include<iostream>
+using namespace std;
 void printPat(int n){
     int temp=n;
-int m=n;
-for(int i=0;i<n;i++){
-    for(int j=0;j<n;j++){
-        for(int z=temp;z>0;z--){
-            cout<<m<<" ";
+    int m=n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            for(int k=temp;k>0;k--){
+                cout<<m<<" ";
+            }
+            m--;
         }
-        m--;
+        cout<<"$";
+        m=n;
+        temp--;
     }
-    cout<<"$";
-    m=n;
-    temp--;
 }
+int main(){
+    int n,res;
+    cout<<"Enter the number of rows : ";
+    cin>>n;
+    res=printpat(n);
+    cout<<res;
+    
+    return 0;
 }
