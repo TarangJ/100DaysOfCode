5898beb8a211a7c247f5f2995889a5de9800b85b
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2022-12-13 21:33:09 +0530

    Update DigRoot.cpp

--- Day49/DigRoot.cpp
+++ Day49/DigRoot.cpp
@@ -1,9 +1,11 @@
-class Solution{
-  public:
+#include<iostream>
+using namespace std;
+int main(){
+  return 0;
+}
     int digitalRoot(int n){
         if(n<=9)
             return n;
             
         return digitalRoot((n/10)+digitalRoot(n%10)); 
     }
-};
