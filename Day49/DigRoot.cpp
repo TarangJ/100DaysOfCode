acb5b9c2a1777645f17fa24065957bf1f87a8173
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2022-12-16 23:48:24 +0530

    Update DigRoot.cpp

--- Day49/DigRoot.cpp
+++ Day49/DigRoot.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 using namespace std;
-int main(){
-  return 0;
-}
-    int digitalRoot(int n){
+int digitalRoot(int n){
         if(n<=9)
             return n;
-            
+  
         return digitalRoot((n/10)+digitalRoot(n%10)); 
-    }
+}
+int main(){
+  int num,dg;
+  cout<<"Enter the number :";
+  cin>>num;
+  dg=digitalRoot(num);
+  cout<<"Digital Root : "<<dg;
+  
+  return 0;
+}
