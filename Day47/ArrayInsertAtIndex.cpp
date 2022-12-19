58ef1fb314c848fd24ed9bb6417d6fd156c2ac19
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2022-12-19 20:55:19 +0530

    Update ArrayInsertAtIndex.cpp

--- Day47/ArrayInsertAtIndex.cpp
+++ Day47/ArrayInsertAtIndex.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 using namespace std;
 int main(){
+    int n,in,e,t;
+    cout<<"Enter the size of array : ";
+    cin>>n;
+    n=sizeof(arr)/sizeo[0];
+    int arr[n];
+    cout<<"Enter the elements : ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter the index value to insert : ";
+    cin>>i;
+    cout<<"Enter element you want to insert : ";
+    cin>>e;
+    t=insertAtIndex(arr,n,in,e);
+    cout<<" "<<t;
+    
     return 0;
 }
 void insertAtIndex(int arr[], int sizeOfArray, int index, int element){
