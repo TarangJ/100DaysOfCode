e7d125aeeb7e43f4e933321d742f82fc10bea998
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2022-12-26 23:37:25 +0530

    Update ValueAtInd.cpp

--- Day50/ValueAtInd.cpp
+++ Day50/ValueAtInd.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 vector<int> valueEqualToIndex(int arr[], int n) {
-	     vector<int>res;
-	    for(int i=0;i<n;i++){
-	        if(arr[i]==i+1)
-	        res.push_back(arr[i]);
-	    }
-	    return res;
+	vector<int>res;
+	for(int i=0;i<n;i++){
+	   if(arr[i]==i+1)
+	    	res.push_back(arr[i]);
+	   }
+	   return res;
 	}
 };
 int main(){
-return 0;
+
+    return 0;
 }
