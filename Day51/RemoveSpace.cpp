8732aa01c98a391bc3d9e7f93c89e5ac71cb96a2
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2022-12-29 23:17:03 +0530

    Update RemoveSpace.cpp

--- Day51/RemoveSpace.cpp
+++ Day51/RemoveSpace.cpp
@@ -1,9 +1,6 @@
-class Solution
-{
-  public:
-    string modify (string s)
-    {
-        //code here.
+#include<iostream>
+using namespace std;
+string modify (string s){
         for(int i=0;i<s.size();i++){
             if(s[i]==32){
                 s[i]=0;
@@ -11,4 +8,12 @@ class Solution
         }
         return s;
     }
-};
+int main(){
+  string r,s;
+  cout<<"Enter the your string :";
+  cin>>s;
+  r=modify(s);
+  cout<<"Space removed : "<<r<<endl;
+  
+  return 0;
+}
