b91f1b9353d9c1ea819dcd02b0455616b68cdd20
Tarang Jaiswal <65700353+TarangJ@users.noreply.github.com>
2023-01-02 23:27:20 +0530

    Update EvenlyDiv.cpp

--- Day52/EvenlyDiv.cpp
+++ Day52/EvenlyDiv.cpp
@@ -1,14 +1,22 @@
-class Solution{
-public:
-    int evenlyDivides(int N){
-        int num=N,rem,op=0;
-        while(num>0){
-            rem=num%10;
-            if(rem!=0 && N%(rem%10)==0){
-                op++;
-            }
-            num/=10;
+#include<iostream>
+using namespace std;
+int main(){
+    int n,res;
+    cout<<"Enter the number : ";
+    cin>>n;
+    res=evenlyDivides(n);
+    cout<<res<<"/n";
+    
+    return 0;
+}
+int evenlyDivides(int N){
+    int num=N,rem,op=0;
+    while(num>0){
+        rem=num%10;
+        if(rem!=0 && N%(rem%10)==0){
+            op++;
         }
-        return op;
+        num/=10;
     }
-};
+    return op;
+}
